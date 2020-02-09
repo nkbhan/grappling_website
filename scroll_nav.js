c4dd0c930d40684c45b54796d22025557cf7a230
Naren Bhandari <nbhandari97@gmail.com>
2020-02-09 11:30:50 -0500

    bigfix to use class name instead of id

--- scroll_nav.js
+++ scroll_nav.js
@@ -2,9 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
+        document.getElementsByClassName("main-nav")[0].style.top = "0";
     } else {
-        document.getElementById("navbar").style.top = "-50px";
+        document.getElementsByClassName("main-nav")[0].style.top = "-50px";
     }
     prevScrollpos = currentScrollPos;
 }
